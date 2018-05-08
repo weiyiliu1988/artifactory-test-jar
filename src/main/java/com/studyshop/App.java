ed7c17a7ba48046f03fed2f170447e17bfee9388
edward <731839038@qq.com>
2018-05-09 07:28:16 +0800

    first commit

---
+++ src/main/java/com/studyshop/App.java
@@ -0,0 +1,12 @@
+package com.studyshop;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+	public static String hello() {
+		return "hello world!";
+	}
+}
